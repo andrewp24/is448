e375c681cfc694fc25f2d57034d6ea369a02b659
Andrew Peterson <andrewp2@umbc.edu>
2018-02-26 12:03:33 -0500

    initial

---
+++ 2495-andrewp2-hw2/display-results.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+    <title>Homework 2: submit.php</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" title="style" />
+</head>
+
+<body>
+    <div class="intro">
+        <h1>Here are the results for popular songs, composers, and artists.</h1>
+	<p>
+        <a href="music.html">Click here to go back to the main page.</a>
+    </p>
+    </div>
+</body>
+
+</html>
