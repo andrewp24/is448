e7bdc6ada122e94492dfc4902a94d1b13fc2074e
Andrew Peterson <andrewp2@umbc.edu>
2018-02-28 17:53:29 -0500

    added post info

--- 2495-andrewp2-hw2/submit.php
+++ 2495-andrewp2-hw2/submit.php
@@ -1,5 +1,10 @@
 <?php
 $myfile = fopen("/afs/umbc.edu/users/a/n/andrewp2/pub/text-files/test.txt", "w") or die("Unable to open file!");
+
+$favSong = $_POST["fav_song"];
+$comp = $_POST["composer"];
+$musicianName = $_POST["musician_name"]
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -16,6 +21,19 @@ $myfile = fopen("/afs/umbc.edu/users/a/n/andrewp2/pub/text-files/test.txt", "w")
     <div class="intro">
         <h1>Thanks for submitting your answers.</h1>
         <p>They have been recorded.</p>
+
+        <p>
+            Favorite song:
+            <strong> <?php echo $favSong ?></strong>
+        </p>
+        <p>
+            Composer:
+            <strong> <?php echo $comp ?></strong>
+        </p>
+        <p>
+            Musician Name:
+            <strong> <?php echo $musicianName ?></strong>
+        </p>
 	<p>
         <a href="music.html">Click here to go back to the main page.</a>
     </p>
