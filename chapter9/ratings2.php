285a7c3bd3409beb039cf888b6852e6fbf3d559f
Andrew Peterson <andrewp2@umbc.edu>
2018-03-05 14:14:14 -0500

    initial commit

---
+++ chapter9/ratings2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="EN">
+  <head>
+	<title>response to customer satisfaction survey</title>
+	<link rel="stylesheet" type="text/css" href="form_proc.css" />
+  </head>
+  <body>
+
+
+	<?php
+
+?>
+
+
+	</body>
+</html>
