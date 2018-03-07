0c252702cd9391b016b4a287f358fc17021dceb1
Andrew Peterson <andrewp2@umbc.edu>
2018-03-07 13:45:09 -0500

    finished

--- chapter9/ratings2.php
+++ chapter9/ratings2.php
@@ -15,13 +15,27 @@ if ((isset($rating) && (!empty($rating)))
 ) {
     $bytes_written = fwrite($filevar2, $out_data);
     flock($filevar2, LOCK_UN);
-
+    echo "Your score of $rating has been recorded. <br />";
     if ($bytes_written == false) {
-        echo ">write failed <br />";
+        echo "write failed <br />";
     }
     ;
 
 }
+
+//now we will read the file.
+$readFile = file("/afs/umbc.edu/users/a/n/andrewp2/pub/text-files/ratings.txt");
+$i = 1;
+$totalRating = 0;
+
+foreach ($readFile as $line) {
+    print "Line $i: $line <br />";
+    $totalRating += $line;
+    $i++;
+}
+$totalLines = $i - 1;
+$avg = $totalRating / $totalLines;
+echo "There are $totalLines total ratings given. The average rating is: $avg";
 
 fclose($filevar2);
 ?>
