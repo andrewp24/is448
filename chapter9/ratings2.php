c5547a6daef2dbb97effdad4ef6fbaaeda4bebf0
Andrew Peterson <andrewp2@umbc.edu>
2018-03-07 13:24:00 -0500

    testing

--- chapter9/ratings2.php
+++ chapter9/ratings2.php
@@ -5,10 +5,25 @@
 	<link rel="stylesheet" type="text/css" href="form_proc.css" />
   </head>
   <body>
-
-
 	<?php
+$filevar2 = fopen("/afs/umbc.edu/users/a/n/andrewp2/pub/text-files/ratings.txt", "a") or die("Unable to open file!");
+$rating = $_POST["rating"];
+$out_data = "$rating \n";
 
+flock($filevar2, LOCK_EX);
+if ((isset($rating) && (!empty($rating)))
+) {
+    $bytes_written = fwrite($filevar2, $out_data);
+    flock($filevar2, LOCK_UN);
+
+    if ($bytes_written == false) {
+        echo ">write failed <br />";
+    }
+    ;
+
+}
+
+fclose($filevar2);
 ?>
 
 
