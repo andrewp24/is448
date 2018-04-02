a5701e96b58e9411a377422c7ab227cbf7fb2a79
Andrew Peterson <andrewp2@umbc.edu>
2018-04-02 13:46:43 -0400

    initial commit

---
+++ chapter13/is448_guestbook.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS guestbook;
+
+CREATE TABLE guestbook (
+	comment_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	username VARCHAR(40), 
+	comment_text VARCHAR(250), 
+	phone_number VARCHAR(15));
+
+INSERT INTO guestbook (username,comment_text,phone_number) VALUES ('tom jones', 'this is a great page', '111-111-1111');
+
+INSERT INTO guestbook (username,comment_text,phone_number) VALUES ('matt parker', 'nice page', '111-222-1111');
+
