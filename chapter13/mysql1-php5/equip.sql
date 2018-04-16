0f29b81ca2009b4e79823bbca3b3f74fcf9ada56
Andrew Peterson <andrewp2@umbc.edu>
2018-04-16 10:36:26 -0400

    formatting

--- chapter13/mysql1-php5/equip.sql
+++ chapter13/mysql1-php5/equip.sql
@@ -1,10 +1,30 @@
 DROP TABLE IF EXISTS equipment;
 
-CREATE TABLE equipment (equip_id INTEGER PRIMARY KEY NOT NULL, equipment_name CHAR(20), car_id INTEGER);   
+CREATE TABLE equipment
+(
+    equip_id INTEGER PRIMARY KEY NOT NULL,
+    equipment_name CHAR(20),
+    car_id INTEGER
+);
 
-INSERT INTO equipment(equip_id, equipment_name, car_id) VALUES (1, 'CD Player', '1');
-INSERT INTO equipment(equip_id, equipment_name, car_id) VALUES (2, 'CD Player', '2');
-INSERT INTO equipment(equip_id, equipment_name, car_id) VALUES (3, 'CD Player', '3');
+INSERT INTO equipment
+    (equip_id, equipment_name, car_id)
+VALUES
+    (1, 'CD Player', '1');
+INSERT INTO equipment
+    (equip_id, equipment_name, car_id)
+VALUES
+    (2, 'CD Player', '2');
+INSERT INTO equipment
+    (equip_id, equipment_name, car_id)
+VALUES
+    (3, 'CD Player', '3');
 
-INSERT INTO equipment(equip_id, equipment_name, car_id) VALUES (4, 'Heated Seats', '1');
-INSERT INTO equipment(equip_id, equipment_name, car_id) VALUES (5, 'Heated Seats', '2');
+INSERT INTO equipment
+    (equip_id, equipment_name, car_id)
+VALUES
+    (4, 'Heated Seats', '1');
+INSERT INTO equipment
+    (equip_id, equipment_name, car_id)
+VALUES
+    (5, 'Heated Seats', '2');
