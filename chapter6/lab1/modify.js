479bdc1cacc7c884ec07ac31c6e45b1bc745b916
Andrew Peterson <andrewp2@umbc.edu>
2018-04-25 14:17:48 -0400

    lab complete

--- chapter6/lab1/modify.js
+++ chapter6/lab1/modify.js
@@ -2,10 +2,29 @@ window.onload = pageLoad;
 
 function pageLoad() {
     var bigTextButton = document.getElementById("bigTextButton");
+    var checkbox = document.getElementById("blingCheckbox");
+
     bigTextButton.onclick = enlarger;
+    checkbox.onchange = blinger;
 }
 
 function enlarger() {
-    alert("button was clicked");
     document.getElementById("placeForText").style.fontSize = "24pt";
+}
+
+function blinger() {
+    textAreaElem = document.getElementById("placeForText")
+    checkbox = document.getElementById("blingCheckbox");
+
+    if (checkbox.checked) {
+        textAreaElem.style.color = "green";
+        textAreaElem.style.textDecoration = "underline";
+        textAreaElem.style.fontWeight = "bold";
+    } else {
+        textAreaElem.style.color = "black";
+        textAreaElem.style.textDecoration = "none";
+        textAreaElem.style.fontWeight = "normal";
+    }
+
+
 }
